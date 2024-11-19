49f84008ee4759f4bfb94aac4b5f32ac9961e725
Malek Arafeh <malek@example.com>
2024-11-19 14:07:32 +0300

    malek

--- example-app/resources/views/user/login.blade.php
+++ example-app/resources/views/user/login.blade.php
@@ -8,8 +8,8 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{url('css/login.css') }}">
-
+<link rel="stylesheet" href="{{url('user/css/login.css') }}">
+link
 
 <style>
         
