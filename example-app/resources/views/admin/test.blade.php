394b633b4a0572723502d8c1b6a06847a7273d28
Hamza Alqurneh <hamzaalqurneh@Hamzas-MacBook-Air.local>
2024-11-19 14:05:38 +0300

    adding folder admin to public

--- example-app/resources/views/admin/test.blade.php
+++ example-app/resources/views/admin/test.blade.php
@@ -12,10 +12,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{url('admin-css/css/plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{url('admin/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{url('admin-css/css/adminlte.min.css') }}">
-  <link rel="stylesheet" href="{{url('admin-css/css/custom.css') }}">
+  <link rel="stylesheet" href="{{url('admin/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{url('admin/css/custom.css') }}">
 
 
 
@@ -283,10 +283,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="{{url('admin/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{url('admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="{{url('admin/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
